c4813102b32cb8ff3a7855a1e76b190223a4f884
lucas.lvn <lucas.lvn97439@gmail.com>
2022-11-22 14:58:14 +0100

    home.js, scroll page to down functions append

---
+++ src/views/js/home.js
@@ -0,0 +1,51 @@
+/* eslint-disable */
+import request from '../../request';
+import Portfolio from '../../components/Home-portfolio.vue'
+
+export default {
+    name: 'HomeView',
+    components: {
+        Portfolio
+    },
+
+    data(){
+        return {
+            pages: [],
+            jsScroll: true,
+        }
+    },
+  
+    mounted(){
+        this.pages = document.querySelectorAll('.page')
+    },
+
+    methods: {
+        timer: function(ms){
+            return new Promise(callback => setTimeout(callback, ms))
+        },
+
+        task: async function(ms){
+            await this.timer(10)
+        } ,
+
+        scrollDown: async function(){
+            const scrollTo = 500
+
+            window.addEventListener('wheel', () => {
+                this.jsScroll = false
+            })
+
+            for (let i = 0; i < scrollTo; i += 2) {
+                if(this.jsScroll){
+                    window.scrollBy(0, 10)
+                    await this.task()
+                }else{
+                    this.jsScroll = true
+                    i += (scrollTo - 300)
+                }
+            }
+
+        },
+
+    }
+}
