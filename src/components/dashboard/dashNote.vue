20aec2766f8cf66ac961fe74774299f10ff9fdc0
lucas.lvn <lucas.lvn97439@gmail.com>
2022-12-26 14:35:58 +0100

    append of dash note component

---
+++ src/components/dashboard/dashNote.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card>
+        <v-card-title primary-title>
+            Note
+        </v-card-title>
+
+        <div class="container-95">
+            sz
+        </div>
+    </v-card>
+</template>
+
+<script src="./js/dasNote.js"></script>
