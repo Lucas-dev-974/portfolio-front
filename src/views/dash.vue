a9a0d21d1b9f1a6e8b3bf29cdcadc1346dd2b7be
lucas.lvn <lucas.lvn97439@gmail.com>
2022-12-26 14:32:40 +0100

    add Project, Post, Note component to dash vue

--- src/views/dash.vue
+++ src/views/dash.vue
@@ -1,4 +1,36 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <div></div>
+    <main  class="full-height-nav container pt-5 ">
+        <div class="d-flex w-100">
+            <div id="sidebar">
+                <v-card min-width="250" theme="custom">
+                    <v-card-title primary-title>
+                        Générale
+                    </v-card-title>
+
+                    <div class="">
+                        NB visite today: X
+                    </div>
+                </v-card>
+            </div>
+            
+            <div id="dash_pages" class="mx-2 d-flex w-100">
+                <div class="dashpage_item w-100" v-if="$store.state.dash.on_page == 'project'">
+                    <Project />
+                </div>
+
+                <div class="dashpage_item w-100" v-if="$store.state.dash.on_page == 'post'">
+                    <Post />
+                </div>
+
+                <div class="dashpage_item w-100" v-if="$store.state.dash.on_page == 'note'">
+                    <Note />
+                </div>
+            </div>
+        </div>
+
+        <btmNavigation />
+    </main>
 </template>
+
+<script src="./js/dash.js"></script>
--- src/views/js/dash.js
+++ src/views/js/dash.js
@@ -1,4 +1,12 @@
+import btmNavigation from '../../components/dashBottomNavigation.vue'
+import Project from '../../components/dashboard/dashProject.vue'
+import Note    from '../../components/dashboard/dashNote.vue'
+import Post    from '../../components/dashboard/dashPost.vue'
+
 export default{
+
+    components: {btmNavigation, Project, Note, Post},
+    
     data(){
         
     },
