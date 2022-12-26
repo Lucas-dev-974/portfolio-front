2dc0b487686ea8d27247603fbfe86074e1dcf3c8
lucas.lvn <lucas.lvn97439@gmail.com>
2022-12-26 14:35:30 +0100

    append of dash Post component

---
+++ src/components/dashboard/dashPost.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card class="">
+        <v-card-title primary-title>
+            Post
+        </v-card-title>
+
+        <div class="container-95">
+            sz
+        </div>
+    </v-card>
+</template>
+
+<script src="./js/dashPost.js"></script>
