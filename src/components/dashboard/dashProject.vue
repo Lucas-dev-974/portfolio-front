78f5435f8c9324708d410c241cf84fdc1913258a
lucas.lvn <lucas.lvn97439@gmail.com>
2022-12-26 14:35:15 +0100

    append of dash project component

---
+++ src/components/dashboard/dashProject.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-card class="w-100">
+        <v-card-title primary-title>
+            Project
+        </v-card-title>
+
+        <div class="container-95">
+            sz
+        </div>
+    </v-card>
+</template>
+
+<script src="./js/dashProject.js"></script>
